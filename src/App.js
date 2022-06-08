ec710f1f0d3c31558695c4581fa310bdb5573176
Alan Caruso <alan.carusoo@gmail.com>
2022-06-08 01:26:10 -0300

    Adding Bootstrap // Setting Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
+  const [data, setData] = useState(null)
+  function getData(val) {
+    setData(val.target.value);
+    console.log(val.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="card-body">
+          <div className="row">
+            <div className='col'>
+              <label for="formGroupExampleInput" className="form-label">Amount</label>
+              <input type="text" className="form-control" id="formGroupExampleInput" placeholder="1,00" onChange={getData} />
+            </div>
+            <div className='col'>
+              <h2>{data} EURO =</h2>
+              <div className="row">
+                <div className='col'>
+                  <h1>1.3550 Canadian Dollar</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div >
+      </div>
     </div>
   );
 }
