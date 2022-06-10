069dd8df7bf108be91c38238aeaf138eddb246e7
Alan Caruso <alan.carusoo@gmail.com>
2022-06-10 16:16:16 -0300

    request update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
+import axios, { Axios } from "axios";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 function App() {
   const [currency, setCurrency] = useState([]);
-  const [inputValue, setInputValue] = useState(null);
-  const [exchangeValue, setExchangeValue] = useState(null);
+  const [inputValue, setInputValue] = useState();
+  const [exchangeValue, setExchangeValue] = useState();
+  const [toValue, setToValue] = useState();
+  const [fromValue, setFromValue] = useState();
+  const [from, setFrom] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+
 
   useEffect(() => {
-    axios.get("https://api.vatcomply.com/rates?base=USD")
+    axios.get(`https://api.vatcomply.com/rates?base=${from}`)
       .then(res => {
-        setCurrency(res.data.rates.EUR);
-      })
-      .catch(err => {
-        console.log(err);
-      })
+        setCurrency(res.data.rates[toCurrency]);
 
-  }, []);
+      })
+  }, [from]);
+
+  console.log(currency)
 
   useEffect(() => {
     let defaultValue = "1.00";
@@ -26,19 +30,19 @@ function App() {
     convert();
   }, [currency])
 
-
-
-  console.log(exchangeValue)
-
   function getData(val) {
     setInputValue(val.target.value);
-    convert()
+    setExchangeValue(inputValue * currency)
   }
 
   function convert() {
     var rate = currency;
     setExchangeValue(inputValue * rate)
+    setToValue(rate)
+
+
   }
+
   return (
     <div className="App">
       <div className='w-100 d-flex justify-content-center mt-5'>
@@ -50,20 +54,20 @@ function App() {
                 <input type="text" className="form-control" id="formGroupExampleInput" placeholder="1,00" value={inputValue} onChange={getData} />
               </div>
               <div className='col'>
-                <h3>{inputValue} USD =</h3>
+                <h3 className='text-secondary'>{inputValue} USD =</h3>
                 <div className="row">
                   <div className='col'>
-                    <h2>{exchangeValue} EUR</h2>
+                    <h3>{exchangeValue} EUR</h3>
                   </div>
                 </div>
                 <div className="row">
                   <div className='col'>
-                    <h3 className=''>1 CA$ = </h3>
+                    <h5>1 USD = {toValue}</h5>
                   </div>
                 </div>
                 <div className="row">
                   <div className='col'>
-                    <h3 className=''>1 EUR = </h3>
+                    <h5 className=''>1 EUR = {fromValue}</h5>
                   </div>
                 </div>
               </div>
